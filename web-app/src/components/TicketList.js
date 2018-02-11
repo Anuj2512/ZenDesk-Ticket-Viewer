56635b1c18caa6526642fbed239510b12be9fd4c
Anuj Chaudhari <chaudharianuj93@gmail.com>
2018-02-11 09:47:01 -0800

    add loader inside buttons

--- web-app/src/components/TicketList.js
+++ web-app/src/components/TicketList.js
@@ -10,13 +10,19 @@ class TicketList extends Component{
         totalTickets : 0,
         bNextPage : "",
         bPrevPage: "disabled",
-        error: ""
+        error: "",
+        bLoadNext : false,
+        bLoadPrev : false
     }
 
     getTicketList(page){
         API.getTicketList(page)
             .then((resData) => {       
                 console.log(resData);
+
+                this.state.bLoadPrev = false;
+                this.state.bLoadNext = false;
+
                 if(resData.error){
                     this.setState({
                         error: resData.error
@@ -31,8 +37,11 @@ class TicketList extends Component{
                     totalTickets: resData.count,
                     bNextPage: (this.state.page !== totalPages ? "" : "disabled"),
                     bPrevPage: (this.state.page !== 1 ? "" : "disabled"),
-                    error: null
+                    error: null,
+                    bLoadPrev: false,
+                    bLoadNext: false
                 })
+
             });
     }
 
@@ -48,15 +57,20 @@ class TicketList extends Component{
 
     onPreviousClicked = (event) => {        
         this.setState({
-            page: this.state.page - 1
-        });
-        console.log(event);
+            page: this.state.page - 1,
+            bNextPage: "disabled",
+            bPrevPage: "disabled",
+            bLoadPrev: true
+        });        
     };
 
     onNextClicked = (event) => {              
         this.setState({
-            page: this.state.page + 1
-        });          
+            page: this.state.page + 1,
+            bNextPage: "disabled",
+            bPrevPage: "disabled",
+            bLoadNext: true
+        });                  
     };
 
     render(){
@@ -77,12 +91,19 @@ class TicketList extends Component{
         let startId = this.state.tickets[0] ? this.state.tickets[0].id : 0;
         let endId = this.state.tickets[this.state.tickets.length-1] ? this.state.tickets[this.state.tickets.length-1].id : 0;
         
+        console.log("Prev:", this.state.bLoadPrev);
+        console.log("Next:", this.state.bLoadNext);
+
         return(
             <div className="offset-md-2 col-md-8">    
             <div className="row infoBox">  
-                <div className="col-md-2 text-center"><button className="button btn-primary navigation-button" disabled={this.state.bPrevPage} onClick={(event) => this.onPreviousClicked(event)}>Previous</button></div>
+                <div className="col-md-2 text-center">
+                    <button className="button btn-primary navigation-button" disabled={this.state.bPrevPage} onClick={(event) => this.onPreviousClicked(event)}> {this.state.bLoadPrev ? <i className='fa fa-circle-o-notch fa-spin'></i> : ""} Previous</button>
+                </div>
                 <div className="col-md-8 text-center subjectName"> Showing {startId} - {endId} of {this.state.totalTickets} Tickets</div>
-                <div className="col-md-2 text-center"><button className="button btn-primary navigation-button" disabled={this.state.bNextPage} onClick={(event) => this.onNextClicked(event)}>Next</button></div>
+                <div className="col-md-2 text-center">
+                    <button className="button btn-primary navigation-button" disabled={this.state.bNextPage} onClick={(event) => this.onNextClicked(event)}> {this.state.bLoadNext ? <i className='fa fa-circle-o-notch fa-spin'></i> : ""} Next</button>
+                </div>
             </div>
                 {rows}
             </div>
